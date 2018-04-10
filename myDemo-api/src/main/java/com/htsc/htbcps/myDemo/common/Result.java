5e6cb811216baf09bfd54010efd06f3c9fa6318d
Meles <haishanggudao@126.com>
2018-04-10 15:09:32 +0800

    提交json解析demo

---
+++ myDemo-api/src/main/java/com/htsc/htbcps/myDemo/common/Result.java
@@ -0,0 +1,42 @@
+package com.htsc.htbcps.myDemo.common;
+
+public class Result<T> {
+
+    private String resultcode;
+    private String reason;
+    private T result;
+    private int error_code;
+
+    public String getResultcode() {
+        return resultcode;
+    }
+
+    public void setResultcode(String resultcode) {
+        this.resultcode = resultcode;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public T getResult() {
+        return result;
+    }
+
+    public void setResult(T result) {
+        this.result = result;
+    }
+
+    public int getError_code() {
+        return error_code;
+    }
+
+    public void setError_code(int error_code) {
+        this.error_code = error_code;
+    }
+
+}
--- myDemo-impl/src/main/java/com/htsc/htbcps/myDemo/services/impl/SayHelloServiceImpl.java
+++ myDemo-impl/src/main/java/com/htsc/htbcps/myDemo/services/impl/SayHelloServiceImpl.java
@@ -2,9 +2,13 @@ package com.htsc.htbcps.myDemo.services.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.htsc.htbcps.myDemo.common.Result;
 import com.htsc.htbcps.myDemo.dal.dao.UserModelMapper;
 import com.htsc.htbcps.myDemo.services.SayHelloService;
 import com.htsc.htbcps.myDemo.dal.model.UserModel;
+import com.htsc.htbcps.myDemo.dto.AreaInfoDto;
 import com.htsc.htbcps.myDemo.dto.User;
 
 public class SayHelloServiceImpl implements SayHelloService {
@@ -24,6 +28,11 @@ public class SayHelloServiceImpl implements SayHelloService {
 		user.setUserId(userModel.getUserId());
 		user.setUsername(userModel.getUsername());
 		user.setPassword(userModel.getPassword());
+		
+		String json="{\"resultcode\":\"200\",\"reason\":\"Return Successd!\",\"result\":{\"province\":\"北京\",\"city\":\"\",\"areacode\":\"010\",\"zip\":\"100000\",\"company\":\"联通\",\"card\":\"\"},\"error_code\":0}";
+		Result<JSON> result=JSONObject.parseObject(json,Result.class);  
+		AreaInfoDto areaInfoDto =JSONObject.toJavaObject(result.getResult(),AreaInfoDto.class); 
+	       
 		return user;
 	}
 
